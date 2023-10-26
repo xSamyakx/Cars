b4ea4afc4983a89ccfec3f0e8335d6f4377a5ed1
xSamyakx <samyakjain37@gmail.com>
2023-10-26 18:44:30 +0530

    Carss App

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Team
+from carss.models import Car
 # Create your views here.
 def home(request):
     teams=Team.objects.all()
+    featured_cars=Car.objects.order_by("-created_date").filter(is_featured=True)    
+    all_cars=Car.objects.order_by("-created_date")
     data={
+        'featured_cars':featured_cars,
         'teams':teams,
+        'all_cars':all_cars,
     }
     return render(request,'pages/home.html',data)
 
